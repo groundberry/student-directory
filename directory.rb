b3aead28fe4393adf64110c8886a56bad4ee3b0b
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-09-14 11:35:35 +0100

    Save data to a file

--- directory.rb
+++ directory.rb
@@ -14,16 +14,10 @@ def input_students
       cohort = gets.chomp
       cohort = "Sep2016" if cohort.empty?
     end
-    puts "Please enter the student's hobbies:"
-    hobbies = gets.chomp
-    puts "Please enter the student's country of birth:"
-    country_of_birth = gets.chomp
     # add the student hash to the array
     @students << {
       name: name,
-      cohort: cohort.gsub(/\s+/,"").capitalize.to_sym,
-      hobbies: hobbies,
-      country_of_birth: country_of_birth
+      cohort: cohort.gsub(/\s+/,"").capitalize.to_sym
     }
     puts "Now we have #{@students.count} great student#{@students.count == 1 ? '' : 's'}"
   end
@@ -37,9 +31,7 @@ end
 def print_students_list
   @students.each do |student|
     puts "#{student[:name]}".center(20) +
-    "(#{student[:cohort]} cohort)".center(20) +
-    "- Hobbies: #{student[:hobbies]}".center(20) +
-    "- Country: #{student[:country_of_birth]}".center(20)
+    "(#{student[:cohort]} cohort)".center(20)
     puts
   end
 end
@@ -53,6 +45,7 @@ def print_menu
   puts "What do you wan to do? Select one of the following options:"
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -62,12 +55,26 @@ def show_students
   print_footer
 end
 
+def save_students
+  # opne the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(", ")
+    file.puts(csv_line)
+  end
+  file.close
+end
+
 def process(selection)
   case selection
     when "1"
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       puts "----------".center(22)
       puts
